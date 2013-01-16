43bdf0e0adf01db279fd0fedf6ca4b2b50d645d6
Christopher Darling <me@christopherdarling.co.uk>
2013-01-16 13:28:32 +0000

    BUGFIX: fixes relation id when save & publishing a new record
    if you create a new record and click save & publish the relationship id isn't recorded in the model.

--- code/VersionedGridFieldDetailForm.php
+++ code/VersionedGridFieldDetailForm.php
@@ -145,6 +145,7 @@ class VersionedGridFieldDetailForm_ItemRequest extends GridFieldDetailForm_ItemR
 
 		$form->saveInto($record);
 		$record->write();
+		$this->gridField->getList()->add($record);
 		$record->publish("Stage", "Live");
 
 		$message = sprintf(
