2d1b5d521b0dbdcd0a464285babe52ea2e0487f8
Bram de Leeuw <bram-de-leeuw@hotmail.com>
2017-01-03 10:21:07 +0100

    reintroduced the files more defined as a extension calss

---
+++ code/VersionedModelAdminExtension.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * VersionedModelAdminExtension
+ * replaces the scaffolded gridfield for versioned objects with a VersionedGridFieldDetailForm
+ * See README for details 
+ *
+ * @author Tim Klein, Dodat Ltd <tim[at]dodat[dot]co[dot]nz>
+ */
+class VersionedModelAdminExtension extends Extension {
+
+	function onBeforeInit() {
+		Versioned::reading_stage('Stage');
+	}
+
+	function updateEditForm($form) {
+		$fieldList = $form->Fields();
+
+		foreach($fieldList as $field) {
+			if($field instanceof GridField) {
+				$class = $field->getList()->dataClass();
+				if($class::has_extension("Versioned")) {
+					$config = $field->getConfig();
+					$config->removeComponentsByType('GridFieldDeleteAction')
+						->removeComponentsByType('GridFieldDetailForm')
+						->addComponents(new VersionedGridFieldDetailForm());
+					$field->setConfig($config);
+				}
+			}
+		}
+	}
+
+}
